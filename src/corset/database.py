3c6f5806756e6dd5923ff19bb4530c598d971996
Lorenz Kies <lorenz.kies@gmx.de>
2026-02-07 00:08:24 +0100

    add built-in lens databases for quantum control group

--- src/corset/database.py
+++ src/corset/database.py
@@ -1,15 +1,49 @@
-"""Module defining a LensList class for managing collections of lenses."""
+"""Lens database class for managing collections of lenses.
+
+This class can be used to load the lens databases included with Beam Corset.
+Use :func:`LensList.available` to list the available databases
+and :func:`LensList.load` to load a database by name.
+
+The typical naming scheme for these databases is as follows:
+
+.. code-block:: text
+
+    [domain]/[manufacturer]_[lens type]_L[applicable wavelength in nm]_M[total margins in mm]
+
+e.g. ``quantum_control/Thorlabs_BX_L1064_M10`` is a database from the Quantum Control made up of
+Thorlabs bi-convex lenses applicable for 1064 nm light with a total margin (left + right) of 10 mm.
+There are also combined databases, where the manufacturer and lens type are replaced with `Combined`.
+
+The lens type shorthands are as follows:
+
+- ``BX``: Bi-Convex
+- ``BV``: Bi-Concave
+- ``PX``: Plan-Convex
+- ``XP``: Convex-Plan
+- ``PV``: Plan-Concave
+- ``VP``: Concave-Plan
+
+The lenses in these databases adhere to the following conventions:
+
+.. code-block:: text
+
+    [first letter of manufacturer][lens type][nominal focal length in mm]
+
+so a Thorlabs bi-convex lens with a nominal focal length of 50 mm is named `TBC50`.
+This keeps the name short for plotting and to make it easier to index into the lens list by name.
+"""
 
 from collections.abc import Iterable, Iterator
 from dataclasses import dataclass, fields
 from functools import cached_property
 from itertools import chain
 from pathlib import Path
-from typing import cast, overload
+from typing import ClassVar, cast, overload
 
 import numpy as np
 import pandas as pd
 
+import corset
 from corset.serialize import YamlSerializableMixin
 
 from .core import Lens, ThickLens, ThinLens
@@ -25,6 +59,7 @@ class LensList(YamlSerializableMixin):
     """
 
     lenses: list[Lens]  #: Underlying list of lenses
+    _database_dir: ClassVar[Path] = Path(corset.__file__).parent / "databases"
 
     def __post_init__(self):
         self._by_name  # trigger validation  # noqa: B018
@@ -86,7 +121,7 @@ class LensList(YamlSerializableMixin):
             [
                 {
                     "name": lens.name,
-                    "type": "thin" if not isinstance(lens, ThickLens) else "thick",
+                    "type": {ThinLens: "thin", ThickLens: "thick"}[type(lens)],
                     "focal_length": lens.focal_length,
                     "left_margin": lens.left_margin,
                     "right_margin": lens.right_margin,
@@ -152,24 +187,27 @@ class LensList(YamlSerializableMixin):
 
     @classmethod
     def available(cls) -> list[str]:
-        """List available built-in lens lists.
+        """List available built-in lens databases.
 
         Returns:
-            list[str]: Names of available lens lists.
+            list[str]: Names of available lens databases.
         """
-        raise NotImplementedError("Built-in lens library not implemented yet.")
+        return [
+            path.relative_to(cls._database_dir).with_suffix("").as_posix()
+            for path in (cls._database_dir).rglob("*.csv")
+        ]
 
     @classmethod
     def load(cls, name: str) -> "LensList":
-        """Load a lens list from the built-in library.
+        """Load a lens database from the built-in library.
 
         Args:
-            name: Name of the lens list to load.
+            name: Name of the lens database to load.
 
         Returns:
-            LensList: The loaded lens list.
+            LensList: The loaded lens database.
 
         Raises:
-            ValueError: If the lens list name is not found in the library.
+            FileNotFoundError: If the lens database name is not found in the library.
         """
-        raise NotImplementedError("Built-in lens library not implemented yet.")
+        return cls.load_csv(cls._database_dir / f"{name}.csv")
